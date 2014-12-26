da2731c305fb145b3b95843e47e0ab1ec67e3ba5
riccardo <riccardo.to@gmail.com>
2014-12-26 12:11:09 +0100

    I forgot to add 2 files to previous commit:

---
+++ python-rest-service/README.md
@@ -0,0 +1,5 @@
+virtualenv flask
+flask/bin/pip install flask
+
+# References
+- http://blog.miguelgrinberg.com/post/designing-a-restful-api-with-python-and-flask
---
+++ python-rest-service/app.py
@@ -0,0 +1,42 @@
+#!flask/bin/python
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+    
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = filter(lambda t: t['id'] == task_id, tasks)
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+	#thanks http://stackoverflow.com/questions/21791209/arduino-yun-uhttpd-flask-setup
+    app.run(host='0.0.0.0', debug=True, threaded=True)
+
+
+
+
